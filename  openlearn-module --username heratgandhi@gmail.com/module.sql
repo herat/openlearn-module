e12a544ba6f0ee2b7e32c82f8d6820bcc29d1203
heratgandhi@gmail.com <heratgandhi@gmail.com@9e41ed36-cd76-8e7b-d154-435795e4a544>
2011-06-15 14:42:47 +0000

    15th June 2

---  openlearn-module --username heratgandhi@gmail.com/module.sql
+++  openlearn-module --username heratgandhi@gmail.com/module.sql
@@ -22,12 +22,23 @@ INSERT INTO `language_text` VALUES ('en', '_module','update_param','Update Param
 INSERT INTO `language_text` VALUES ('en', '_module','ol_search_open_learn_tool','Search OpenLearn Content Tool',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_module','ol_search_open_learn_text','A sample Search OpenLearn text for detailed homepage.',NOW(),'');
 
+INSERT INTO `language_text` VALUES ('en', '_module','ol_update','Update Now',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_last_update','Last Updated',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_repo_url','Repository URL',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_cron_interval','CRON interval',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_min','minutes',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_change','Change',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_module','ol_max_reco','Maximum Records',NOW(),'');
+
+
+
 #adding feedback messages
 INSERT INTO `language_text` VALUES ('en', '_msgs','AT_FEEDBACK_OL_DB_UPDATED','Database updated successfully',NOW(),'');
 INSERT INTO `language_text` VALUES ('en', '_msgs','AT_FEEDBACK_OL_DB_NOT_UPDATED','Error in updating database',NOW(),'');
+INSERT INTO `language_text` VALUES ('en', '_msgs','AT_FEEDBACK_SETTINGS_CHANGED','Settings changed successfully',NOW(),'');
 
 
-
+#configuration data
 INSERT INTO `config` VALUES ('ol_url','http://openlearn.open.ac.uk/local/oai/oai2.php?verb=ListRecords&metadataPrefix=oai_ilox');
 
 INSERT INTO `config` VALUES ('ol_last_updation',CURDATE());
